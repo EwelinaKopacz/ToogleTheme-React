928e2d44e153c04072911c91d349694c5fe8d21e
Ewelina <ewelina@ekopacz.pl>
2022-11-04 13:12:18 +0100

    added animation

--- src/components/Mode/Mode.styled.js
+++ src/components/Mode/Mode.styled.js
@@ -1,4 +1,12 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
+
+const appear = keyframes`
+    0% { opacity: 0; transform: translateY(-100vw); }
+    25% { opacity: .25;transform: translateY(-50vw); }
+    75% { opacity: .25; transform: translateY(-20vw); }
+    100% { opacity: 1; transform: translateY(0); }
+
+`;
 
 const ModeWrapper = styled.div`
     width: 200px;
@@ -7,6 +15,7 @@ const ModeWrapper = styled.div`
     justify-content: space-evenly;
     align-items: center;
     border: 1px solid ${(props)=>props.theme.fontColor};
+    animation:${appear} 1.5s;
 `;
 
 export default ModeWrapper;
